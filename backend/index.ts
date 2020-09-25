fb3dbae227b4f17982829e26acd3f4dca2abf47c
Simon Nyman <nyman.simon@hotmail.com>
2020-09-25 13:46:50 +0300

    authentication backend WIP

--- backend/index.ts
+++ backend/index.ts
@@ -3,21 +3,22 @@ const app = express()
 app.use(express.json())
 
 // App info
-const CLIENT_ID = "Iv1.7c7e5687129a365a"
-//const CLIENT_SECRET =  "e55d0a2b2f414f42825fd0d1943d956af613e9bc"
+const CLIENT_ID = "Iv1.e8426b3c0727bfb5"
+//const CLIENT_SECRET =  "60ed1f1c3ab76fd50e84a72e2257122b9132ab30"
 
 app.get('/', (_req: any, res: any) => {
     res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/auth/GH', (_req: any, res: any) => {
-  const redirect_uri = 'http://localhost:3001/auth/GHCB'
+// Github authentication
+app.get('/auth/gh', (_req: any, res: any) => {
+  const redirect_uri = 'http://localhost:3001/login/ghcb'
   res.redirect(
     `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${redirect_uri}`
   )
 })
 
-app.get('/auth/GHCB', (_req: any, res: any) => {
+app.get('/login/ghcb', (_req: any, res: any) => {
   res.json({ code: `${_req.query.code}` })
 })
 
